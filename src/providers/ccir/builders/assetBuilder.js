d3706dba780dbbf9785518b336ced7b971ee9cb0
Debraj Das <Debraj.das@craftacademia.com>
2026-07-24 01:21:19 +0530

    feat: implement asset extraction

--- src/providers/ccir/builders/assetBuilder.js
+++ src/providers/ccir/builders/assetBuilder.js
@@ -1,8 +1,27 @@
+import { walk } from "../utils/treeWalker.js";
+
 export default class AssetBuilder {
 
   build(ast) {
 
     const assets = [];
+
+    walk(ast, node => {
+
+      if (node.type !== "ASSET") {
+        return;
+      }
+
+      assets.push({
+        id: node.attributes?.id ?? null,
+        name: node.attributes?.name ?? null,
+        type: node.attributes?.type ?? null,
+        src: node.attributes?.src ?? null,
+        alt: node.attributes?.alt ?? null,
+        attributes: node.attributes ?? {}
+      });
+
+    });
 
     return assets;
 
