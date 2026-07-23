133da033c62400bad1b33d0c5bc94f780cd6f6b0
Debraj Das <Debraj.das@craftacademia.com>
2026-07-24 01:10:31 +0530

    feat: implement character extraction

--- src/providers/ccir/builders/characterBuilder.js
+++ src/providers/ccir/builders/characterBuilder.js
@@ -1,8 +1,25 @@
+import { walk } from "../utils/treeWalker.js";
+
 export default class CharacterBuilder {
 
   build(ast) {
 
     const characters = [];
+
+    walk(ast, node => {
+
+      if (node.type !== "CHARACTER") {
+        return;
+      }
+
+      characters.push({
+        id: node.attributes?.id ?? null,
+        name: node.attributes?.name ?? null,
+        avatar: node.attributes?.avatar ?? null,
+        attributes: node.attributes ?? {}
+      });
+
+    });
 
     return characters;
 
