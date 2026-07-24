553fd9a277326b414e98f9b1b136530c3d445473
Debraj Das <Debraj.das@craftacademia.com>
2026-07-24 19:22:46 +0530

    Phase 7 - Browser Runtime Integration

---
+++ src/runtime/browserRuntime.js
@@ -0,0 +1,46 @@
+import RuntimePlayer from "./runtimePlayer.js";
+import InteractionRenderer from "./interactionRenderer.js";
+
+import MCQInteraction from "./interactions/mcqInteraction.js";
+import MSQInteraction from "./interactions/msqInteraction.js";
+import HotspotInteraction from "./interactions/hotspotInteraction.js";
+import ClickToRevealInteraction from "./interactions/clickToRevealInteraction.js";
+import DragDropInteraction from "./interactions/dragDropInteraction.js";
+import ReflectionInteraction from "./interactions/reflectionInteraction.js";
+import BranchingInteraction from "./interactions/branchingInteraction.js";
+import CaseStudyInteraction from "./interactions/caseStudyInteraction.js";
+
+export default class BrowserRuntime {
+
+    constructor(page) {
+
+        this.page = page;
+
+        this.player = new RuntimePlayer(page);
+
+        this.interactionRenderer = new InteractionRenderer(this);
+
+        this.interactionRenderer.register("MCQ", MCQInteraction);
+        this.interactionRenderer.register("MSQ", MSQInteraction);
+        this.interactionRenderer.register("HOTSPOT", HotspotInteraction);
+        this.interactionRenderer.register("CLICK_TO_REVEAL", ClickToRevealInteraction);
+        this.interactionRenderer.register("DRAG_DROP", DragDropInteraction);
+        this.interactionRenderer.register("REFLECTION", ReflectionInteraction);
+        this.interactionRenderer.register("BRANCHING", BranchingInteraction);
+        this.interactionRenderer.register("CASE_STUDY", CaseStudyInteraction);
+
+    }
+
+    play() {
+
+        return this.player.play();
+
+    }
+
+    renderInteraction(component) {
+
+        return this.interactionRenderer.render(component);
+
+    }
+
+}
