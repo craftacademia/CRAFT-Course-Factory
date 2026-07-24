a6d035fe8ed178cf8a0035554760d85fff74bfec
Debraj Das <Debraj.das@craftacademia.com>
2026-07-24 18:30:48 +0530

    Phase 6 - MCQ Interaction API

---
+++ src/runtime/interactions/mcqInteraction.js
@@ -0,0 +1,62 @@
+import Interaction from "./interaction.js";
+
+export default class MCQInteraction extends Interaction {
+
+    constructor(component, runtime) {
+
+        super(component, runtime);
+
+        this.answer = null;
+
+    }
+
+    render() {
+
+        return this.component.properties.options
+            .map((option, index) => `
+<label>
+    <input
+        type="radio"
+        name="${this.component.id}"
+        value="${index}">
+    ${option}
+</label><br>`)
+            .join("");
+
+    }
+
+    collect(value) {
+
+        this.answer = Number(value);
+
+        return this.answer;
+
+    }
+
+    validate() {
+
+        return this.answer !== null;
+
+    }
+
+    evaluate() {
+
+        return this.answer === this.component.properties.correct;
+
+    }
+
+    feedback() {
+
+        return this.evaluate()
+            ? this.component.properties.correctFeedback
+            : this.component.properties.incorrectFeedback;
+
+    }
+
+    complete() {
+
+        return this.validate();
+
+    }
+
+}
