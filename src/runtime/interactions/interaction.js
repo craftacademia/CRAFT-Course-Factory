c38cda9fb43ad9b0521f037b806eaef18610b494
Debraj Das <Debraj.das@craftacademia.com>
2026-07-24 18:23:45 +0530

    Phase 6 - Interaction Base

---
+++ src/runtime/interactions/interaction.js
@@ -0,0 +1,46 @@
+export default class Interaction {
+
+    constructor(component, runtime) {
+
+        this.component = component;
+        this.runtime = runtime;
+
+    }
+
+    render() {
+
+        throw new Error("render() not implemented.");
+
+    }
+
+    collect() {
+
+        return null;
+
+    }
+
+    validate() {
+
+        return true;
+
+    }
+
+    evaluate() {
+
+        return null;
+
+    }
+
+    feedback() {
+
+        return null;
+
+    }
+
+    complete() {
+
+        return true;
+
+    }
+
+}
