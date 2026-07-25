8cfbc1f3507ea3950ee3d123b7ee896dc0013ddd
Debraj Das <Debraj.das@craftacademia.com>
2026-07-25 13:24:05 +0530

    Phase 8 - JavaScript Bundler

--- src/providers/rendering/runtime/runtimeBuilder.js
+++ src/providers/rendering/runtime/runtimeBuilder.js
@@ -2,24 +2,37 @@ import fs from "fs/promises";
 
 export default class RuntimeBuilder {
 
-  async build(outputFile) {
+    async build(outputFile) {
 
-    const runtime = `
-(function(){
+        const runtime = `
+import BrowserRuntime from "./browserRuntime.js";
 
-    console.log("CRA.F.T Runtime Started");
+document.addEventListener("DOMContentLoaded", () => {
 
-    console.log(window.PIR);
+    if (!window.PIR) {
+        console.error("PIR not found.");
+        return;
+    }
 
-})();
+    const runtime = new BrowserRuntime(window.PIR);
+
+    runtime.play();
+
+    window.courseRuntime = runtime;
+
+    console.log("C.R.A.F.T Runtime Started");
+
+});
 `;
 
-    await fs.writeFile(
-      outputFile,
-      runtime,
-      "utf8"
-    );
+        await fs.writeFile(
+            outputFile,
+            runtime,
+            "utf8"
+        );
 
-  }
+        return outputFile;
+
+    }
 
 }
