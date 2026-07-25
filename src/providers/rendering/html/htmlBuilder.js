ff7dba3b1d7643556d0b7f93af937a0d4607af62
Debraj Das <Debraj.das@craftacademia.com>
2026-07-25 13:15:33 +0530

    Phase 8 - HTML Generator

--- src/providers/rendering/html/htmlBuilder.js
+++ src/providers/rendering/html/htmlBuilder.js
@@ -5,45 +5,65 @@ import CourseRenderer from "./courseRenderer.js";
 
 export default class HtmlBuilder {
 
-  constructor() {
-    this.courseRenderer = new CourseRenderer();
-  }
+    constructor() {
 
-  async build(pir, outputDirectory) {
+        this.courseRenderer = new CourseRenderer();
 
-    await fs.mkdir(outputDirectory, { recursive: true });
+    }
 
-    const body = this.courseRenderer.render(pir);
+    async build(pir, outputDirectory) {
 
-    const html = `<!DOCTYPE html>
+        await fs.mkdir(outputDirectory, {
+            recursive: true
+        });
+
+        const body = this.courseRenderer.render(pir);
+
+        const html = `<!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<title>${pir?.course?.title ?? "CRA.F.T Course"}</title>
+
+<meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+>
+
+<title>${pir?.course?.title ?? "C.R.A.F.T Course"}</title>
+
 <link rel="stylesheet" href="styles.css">
+
 </head>
+
 <body>
 
 <div id="app">
+
 ${body}
+
 </div>
 
 <script>
-window.PIR=${JSON.stringify(pir)};
+
+window.PIR = ${JSON.stringify(pir, null, 2)};
+
 </script>
 
-<script src="runtime.js"></script>
+<script type="module" src="runtime.js"></script>
 
 </body>
+
 </html>`;
 
-    await fs.writeFile(
-      path.join(outputDirectory, "index.html"),
-      html,
-      "utf8"
-    );
+        await fs.writeFile(
+            path.join(outputDirectory, "index.html"),
+            html,
+            "utf8"
+        );
 
-  }
+        return path.join(outputDirectory, "index.html");
+
+    }
 
 }
