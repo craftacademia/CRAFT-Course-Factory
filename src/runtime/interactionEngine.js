f355053b1bda03d38277ad30bcad04a10cab00ec
Debraj Das <Debraj.das@craftacademia.com>
2026-07-26 06:50:08 +0530

    M6: Complete interaction engine integration

--- src/runtime/interactionEngine.js
+++ src/runtime/interactionEngine.js
@@ -13,6 +13,114 @@ export default class InteractionEngine {
         this.eventBus = eventBus;
         this.runtimeState = runtimeState;
 
+        this.handlers = new Map();
+        this.middleware = [];
+
+        this.beforeExecuteHooks = [];
+        this.afterExecuteHooks = [];
+
+        this.registerDefaults();
+
+    }
+
+    registerDefaults() {
+
+        this.register("SET_VARIABLE", interaction => {
+
+            this.runtimeState.variables.set(
+                interaction.name,
+                interaction.value
+            );
+
+        });
+
+        this.register("ADD_SCORE", interaction => {
+
+            this.runtimeState.addScore(
+                Number(interaction.points ?? 0)
+            );
+
+        });
+
+        this.register("SET_PAGE", interaction => {
+
+            this.runtimeState.setPage(
+                Number(interaction.page ?? 0)
+            );
+
+        });
+
+        this.register("COMPLETE_COURSE", () => {
+
+            this.runtimeState.complete();
+
+        });
+
+        this.register("EMIT_EVENT", interaction => {
+
+            this.eventBus.emit(
+                interaction.event,
+                interaction.payload ?? {}
+            );
+
+        });
+
+    }
+
+    use(middleware) {
+
+        if (typeof middleware !== "function") {
+            throw new Error("Middleware must be a function.");
+        }
+
+        this.middleware.push(middleware);
+
+    }
+
+    beforeExecute(hook) {
+
+        if (typeof hook !== "function") {
+            throw new Error("Hook must be a function.");
+        }
+
+        this.beforeExecuteHooks.push(hook);
+
+    }
+
+    afterExecute(hook) {
+
+        if (typeof hook !== "function") {
+            throw new Error("Hook must be a function.");
+        }
+
+        this.afterExecuteHooks.push(hook);
+
+    }
+
+    register(type, handler) {
+
+        if (typeof type !== "string" || !type) {
+            throw new Error("Interaction type is required.");
+        }
+
+        if (typeof handler !== "function") {
+            throw new Error("Interaction handler must be a function.");
+        }
+
+        this.handlers.set(type, handler);
+
+    }
+
+    unregister(type) {
+
+        this.handlers.delete(type);
+
+    }
+
+    has(type) {
+
+        return this.handlers.has(type);
+
     }
 
     execute(interaction) {
@@ -21,54 +129,24 @@ export default class InteractionEngine {
             throw new Error("Interaction type is required.");
         }
 
-        switch (interaction.type) {
+        for (const hook of this.beforeExecuteHooks) {
+            hook(interaction, this.runtimeState, this.eventBus);
+        }
 
-            case "SET_VARIABLE":
+        for (const middleware of this.middleware) {
+            middleware(interaction, this.runtimeState, this.eventBus);
+        }
 
-                this.runtimeState.variables.set(
-                    interaction.name,
-                    interaction.value
-                );
+        const handler = this.handlers.get(interaction.type);
 
-                break;
+        if (!handler) {
+            throw new Error(`Unknown interaction: ${interaction.type}`);
+        }
 
-            case "ADD_SCORE":
+        handler(interaction);
 
-                this.runtimeState.addScore(
-                    Number(interaction.points ?? 0)
-                );
-
-                break;
-
-            case "SET_PAGE":
-
-                this.runtimeState.setPage(
-                    Number(interaction.page ?? 0)
-                );
-
-                break;
-
-            case "COMPLETE_COURSE":
-
-                this.runtimeState.complete();
-
-                break;
-
-            case "EMIT_EVENT":
-
-                this.eventBus.emit(
-                    interaction.event,
-                    interaction.payload ?? {}
-                );
-
-                break;
-
-            default:
-
-                throw new Error(
-                    `Unknown interaction: ${interaction.type}`
-                );
-
+        for (const hook of this.afterExecuteHooks) {
+            hook(interaction, this.runtimeState, this.eventBus);
         }
 
         return true;
