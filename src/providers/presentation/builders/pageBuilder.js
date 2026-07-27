c7106a673d40526c784b842be894c0de80f40a88
Debraj Das <Debraj.das@craftacademia.com>
2026-07-28 04:03:22 +0530

    feat(asset): resolve image assets into presentation components

--- src/providers/presentation/builders/pageBuilder.js
+++ src/providers/presentation/builders/pageBuilder.js
@@ -1,215 +1,39 @@
-import LayerBuilder from "./layerBuilder.js";
-import ComponentBuilder from "./componentBuilder.js";
+resolveImage(
+    screen,
+    images
+) {
 
-export default class PageBuilder {
+    if (screen.assetRef?.src) {
 
-    constructor() {
-
-        this.layerBuilder =
-            new LayerBuilder();
-
-        this.componentBuilder =
-            new ComponentBuilder();
+        return screen.assetRef.src;
 
     }
 
 
-    build(ccir) {
-
-        const pages = [];
-
-        const images =
-            ccir.assets?.images ?? [];
-
-        const backgroundAudio =
-            ccir.assets?.audio?.background?.[0]?.path ?? null;
+    const scene =
+        screen.attributes?.scene ??
+        null;
 
 
-        for (const screen of ccir.screens ?? []) {
+    const image =
+        images.find(
+            item => {
 
-            const components = [];
+                const imageId =
+                    item.name?.replace(
+                        /\.[^/.]+$/,
+                        ""
+                    );
 
 
-            const image =
-                this.resolveImage(
-                    screen,
-                    images
-                );
-
-
-            if (image) {
-
-                components.push(
-
-                    this.componentBuilder.build(
-                        "IMAGE",
-                        {
-
-                            id:
-                            `IMAGE_${components.length + 1}`,
-
-                            asset:
-                            image,
-
-                            properties:
-                            {
-                                asset:
-                                image
-                            }
-
-                        }
-                    )
-
+                return (
+                    imageId === scene
                 );
 
             }
+        );
 
 
-
-            if (backgroundAudio) {
-
-                components.push(
-
-                    this.componentBuilder.build(
-                        "AUDIO",
-                        {
-
-                            id:
-                            `AUDIO_${components.length + 1}`,
-
-                            asset:
-                            backgroundAudio,
-
-                            properties:
-                            {
-                                asset:
-                                backgroundAudio
-                            }
-
-                        }
-                    )
-
-                );
-
-            }
-
-
-
-            this.addNodeComponents(
-                screen,
-                components
-            );
-
-
-            pages.push({
-
-                id:
-                screen.id,
-
-                title:
-                screen.title ?? "",
-
-                layers:
-                [
-                    this.layerBuilder.build(
-                        "CONTENT",
-                        components
-                    )
-                ]
-
-            });
-
-        }
-
-
-        return pages;
-
-    }
-
-
-
-    resolveImage(
-        screen,
-        images
-    ) {
-
-        if (screen.assetRef?.src) {
-
-            return screen.assetRef.src;
-
-        }
-
-
-        const image =
-            images.find(
-                item => {
-
-                    const imageId =
-                        item.name
-                            ?.replace(
-                                /\.[^/.]+$/,
-                                ""
-                            );
-
-
-                    return imageId === screen.id;
-
-                }
-            );
-
-
-        return image?.path ?? null;
-
-    }
-
-
-
-    addNodeComponents(
-        node,
-        components
-    ) {
-
-        if (!node) {
-
-            return;
-
-        }
-
-
-        if (node.type === "TEXT") {
-
-            components.push(
-
-                this.componentBuilder.build(
-                    "NARRATION",
-                    {
-
-                        id:
-                        `NARRATION_${components.length + 1}`,
-
-                        properties:
-                        {
-                            text:
-                            node.value ?? ""
-                        }
-
-                    }
-                )
-
-            );
-
-        }
-
-
-        for (const child of node.children ?? []) {
-
-            this.addNodeComponents(
-                child,
-                components
-            );
-
-        }
-
-    }
+    return image?.path ?? null;
 
 }
