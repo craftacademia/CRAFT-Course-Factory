49da2ff995ea674e6507a06ccf1b4d832c663b76
Debraj Das <Debraj.das@craftacademia.com>
2026-07-27 18:23:01 +0530

    feat(assets): preserve asset manifest in PIR

--- src/providers/presentation/presentationProvider.js
+++ src/providers/presentation/presentationProvider.js
@@ -1,13 +1,41 @@
 import CourseBuilder from "./builders/courseBuilder.js";
+import TimelineBuilder from "../../runtime/builders/timelineBuilder.js";
 
 export default class PresentationProvider {
 
     constructor() {
+
         this.courseBuilder = new CourseBuilder();
+
+        this.timelineBuilder = new TimelineBuilder();
+
     }
 
+
     async build(ccir) {
-        return this.courseBuilder.build(ccir);
+
+        const presentation =
+            await this.courseBuilder.build(ccir);
+
+
+        for (const page of presentation.pages ?? []) {
+
+            page.timeline =
+                this.timelineBuilder.build(page);
+
+        }
+
+
+        if (ccir.assets) {
+
+            presentation.assets =
+                ccir.assets;
+
+        }
+
+
+        return presentation;
+
     }
 
 }
